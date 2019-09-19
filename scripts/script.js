52cf9ebda6b14b3a0ee697cde8debbefdc7166da
Jimmy Johansson <jim-joh@outlook.com>
2019-09-19 22:54:38 +0200

    css...

---
+++ scripts/script.js
@@ -0,0 +1,15 @@
+
+function initMap() {
+    // The location of Tallbacka
+    var tallBacka = { lat: 57.929239, lng: 13.964798 };
+    // The map, centered at Tallbacka
+    var map = new google.maps.Map(
+        document.getElementById('map'), { zoom: 8, center: tallBacka });
+    // The marker, positioned at Tallbacka
+    var marker = new google.maps.Marker({ position: tallBacka, map: map, title: 'Här bor jag!!' });
+    
+}
+
+
+
+
