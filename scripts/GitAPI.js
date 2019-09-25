0fc011163cf1306be158e619ed636d54f8b37512
Jimmy Johansson <jim-joh@outlook.com>
2019-09-25 12:58:51 +0200

    fixed json conversion

--- scripts/GitAPI.js
+++ scripts/GitAPI.js
@@ -1,33 +1,49 @@
 //"Fetching" repository data from Github
+const repos = "https://api.github.com/users/Tallbacka/repos";
 
-async function getRepos(){
-    const repos = "https://api.github.com/users/Tallbacka/repos";
+async function getRepos() {
     let response = await fetch(repos);
-    let json = await response.json()
-    return json;
+    let data = await response.json()
+    return data;
 }
 
 getRepos()
-.then(json => drawRepoData(json))
-.catch(err => console.log(err + ": Unable to connect to server"))
+    .then(data => drawRepoData(data))
+    .then(data => console.log(data))
+    .catch(err => console.log(err));
 
-function drawRepoData(json){
-    var pName = document.createElement('p');
-    var li = document.createElement('li');
+// Sort by last changed date
 
-    var ul = document.getElementById("listContainer");
+function drawRepoData(data) {
 
-    for (let i = 0; i < json.count; i++) {
-        pName.innerHTML =   json.name[i] + "<br><br>" + 
-                            json.description[i] + "<br><br>" + 
-                            json.html_url[i] + "<br><br>" +
-                            json.updated_at
+    convertAndSortDate(data)
 
+    console.log(data);
+    var ul = document.getElementById("listRepos");
 
+    for (let i = 0; i < Object.values(data).length; i++) {
+        var pName = document.createElement('p');
+        var li = document.createElement('li');
+        var a = document.createElement('a');
+        var insert = document.createElement('i')
 
+        insert.setAttribute('class', "fab fa-github-square");
+
+        a.setAttribute('href', data[i].html_url);
+        a.appendChild(insert);
+
+        pName.innerHTML = "Projektnamn: " + data[i].name + "<br>" + "Senast ändrad: " + data[i].updated_at;
+        pName.setAttribute = ("class", "paragraphName");
+        pName.insertAdjacentElement("afterbegin", a)
+        li.appendChild(pName);
+        ul.appendChild(li);
     }
+}
 
+function convertAndSortDate(data) {
+    var dateJson = JSON.stringify(data[0].updated_at)
+    var date = new date(dateJson)
 
-    // navbar-nav
-    console.log(json);
+    // data.sort(function (a, b) { return a.updated_at - b.updated_at });
+    console.log(date);
 }
