de621d7739121e88161a6727b53df4d49f0990e3
Jimmy Johansson <jim-joh@outlook.com>
2019-09-25 15:52:52 +0200

    cleaning

--- scripts/GitAPI.js
+++ scripts/GitAPI.js
@@ -1,24 +1,19 @@
 //"Fetching" repository data from Github
 const repos = "https://api.github.com/users/Tallbacka/repos";
 
+// Using an asynch function so the rest of the operations can continue
 async function getRepos() {
     let response = await fetch(repos);
     let data = await response.json()
     return data;
-}
-
-getRepos()
+} getRepos() //Using anonomys functions and promises to retrieve and validate data.
     .then(data => drawRepoData(data))
     .then(data => console.log(data))
     .catch(err => console.log(err));
 
-// Sort by last changed date
-
+// creates the elements and populates them with data
 function drawRepoData(data) {
 
-    convertAndSortDate(data)
-
-    console.log(data);
     var ul = document.getElementById("listRepos");
 
     for (let i = 0; i < Object.values(data).length; i++) {
@@ -46,11 +41,4 @@ function drawRepoData(data) {
 
         ul.appendChild(li);
     }
-}
-
-function convertAndSortDate(data) {
-    var lastUpdated = new Date(data[0].updated_at)
-    var output = lastUpdated.getDay() + "/" + lastUpdated.getMonth() + "/" + lastUpdated.getFullYear();
-    data.sort(function (a, b) { return a.updated_at - b.updated_at });
-    console.log(data);
 }
